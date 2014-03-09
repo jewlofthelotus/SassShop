1f927ffc60bf1a2af2e2b3ce0517215c351277e5
Julie Cameron <jewlofthelotus@gmail.com>
2014-03-09 18:29:26 -0400

    began starter project; noted todos in slides

---
+++ exercises/kittens/js/application.js
@@ -0,0 +1,23 @@
+var names = ['Tiger', 'Samuel', 'Leeloo',
+             'Bella', 'Mo', 'Sly',
+             'Beezy', 'Maple', 'Rodney',
+             'Yonce', 'Reginald', 'Winifred'];
+
+var ages  = ['9 Weeks', '12 Weeks', '3 Months',
+             '8 Weeks', '1 Year', '15 Weeks',
+             '4 Months', '2 Months', '14 Weeks',
+             '6 Months', '10 Weeks', '8 Months'];
+
+$('.kittens').find('li').each( function(i, el) {
+  console.log(i);
+  var img = $(el).find('img');
+  var name = $(el).find('.name');
+  var age = $(el).find('.age');
+
+  var w = 250;
+  var h = 250;
+
+  img.attr('src', 'http://placekitten.com/'+w+'/'+h+'?image='+i);
+  name.text(names[i]);
+  age.text(ages[i]);
+});
